ec6822ca19be9d2a5d4b495c51a4617b305adbfc
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-11-09 20:55:43 -0500

    Backend pivot -> use hono instead of express

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+// https://hono.dev/docs/getting-started/nodejs
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const port = Number(process.env.PORT) || 3000;
+const app = new Hono();
+
+app.use(
+  "/*",
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.get("/", (c) => c.text("Hello Node.js!"));
+
+console.log(`Backend server running on http://localhost:${port}`);
+
+serve({ fetch: app.fetch, port });
