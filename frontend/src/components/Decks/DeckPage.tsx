02e2d2f1487eb5345f16584b7a301b8e98c564c5
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-11-22 10:53:31 -0500

    Add delete button on deck page

--- frontend/src/components/Decks/DeckPage.tsx
+++ frontend/src/components/Decks/DeckPage.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import AuthContext from "@contexts/AuthContext";
 import { usePathSegment } from "@customHooks/usePathSegment";
-import { getDeckOfCards } from "@lib/decks";
+import { deleteDeckOfCards, getDeckOfCards } from "@lib/decks";
 import type { CardNoUserId, DeckNoUserId } from "@utils/types";
 
 const DeckPage = () => {
@@ -13,21 +13,23 @@ const DeckPage = () => {
   const [deckInfo, setDeckInfo] = useState<DeckNoUserId>();
   const [cards, setCards] = useState<CardNoUserId[]>([]);
   const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState("");
 
   useEffect(() => {
     const fetchDecks = async () => {
-      const token = auth?.session?.access_token;
+      const accessToken = auth?.session?.access_token;
 
-      if (!token) {
+      if (!accessToken) {
         navigate("/login");
         return;
       }
 
       try {
-        const response = await getDeckOfCards(deckId, token);
+        const response = await getDeckOfCards(deckId, accessToken);
         const { info, cards } = response.data;
         setDeckInfo(info);
         setCards(cards);
+        setToken(accessToken);
         setLoading(false);
       } catch (err) {
         console.error("Failed to load deck:", err);
@@ -36,6 +38,11 @@ const DeckPage = () => {
     };
     fetchDecks();
   }, [auth?.session]);
+
+  const handleDelete = async (id: number) => {
+    await deleteDeckOfCards(id, token);
+    navigate("/decks");
+  };
 
   if (loading) return <p>Loading</p>;
 
@@ -46,6 +53,13 @@ const DeckPage = () => {
       <p>Learning: {deckInfo?.targetLanguage}</p>
       <p>Notes: {deckInfo?.notes}</p>
       <p>Total cards: {deckInfo?.totalCards}</p>
+      <span>
+        Delete this deck
+        <i
+          className="fa-solid fa-trash"
+          onClick={() => handleDelete(Number(deckId))}
+        ></i>
+      </span>
 
       <button onClick={() => navigate(`/decks/${deckId}/new`)}>
         Add New Card
