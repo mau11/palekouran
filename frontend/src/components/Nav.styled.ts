8d198a23680d1ceab1171d857a2ea8a9d3d819eb
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-11-25 04:56:08 -0500

    Update nav styling

--- frontend/src/components/Nav.styled.ts
+++ frontend/src/components/Nav.styled.ts
@@ -1,5 +1,107 @@
 import styled from "styled-components";
+import { Link } from "react-router";
+import breakpoints from "@utils/breakpoints";
 
-export const NavHeader = styled.header`
-  margin-bottom: 2em;
+export const Header = styled.header`
+  background-color: var(--white);
+  box-shadow: 0 0 8px var(--accent);
+`;
+
+export const NavBar = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 90vw;
+  margin: auto;
+  padding: 1em 0;
+
+  i {
+    color: var(--accent);
+  }
+`;
+
+export const NavLink = styled(Link)`
+  text-decoration: none;
+
+  h1 {
+    font-size: 1.5em;
+    font-weight: 600;
+    color: var(--accent);
+    display: flex;
+    gap: 0.25em;
+    align-items: center;
+  }
+
+  &:hover {
+    transform: scale(1.03);
+  }
+`;
+
+export const NavSection = styled.section<{ $open: boolean }>`
+  display: flex;
+  align-items: center;
+  position: relative;
+  gap: 1em;
+
+  @media ${breakpoints.mobile.large} {
+    display: ${({ $open }) => ($open ? "flex" : "none")};
+    flex-direction: column;
+    position: absolute;
+    top: 50px;
+    left: 0;
+    right: 0;
+    background-color: var(--white);
+    padding: 1em 0;
+    box-shadow: 0 5px 10px -5px var(--light-grey);
+    z-index: 10;
+  }
+`;
+
+export const Burger = styled.div`
+  display: none;
+  flex-direction: column;
+  gap: 5px;
+  cursor: pointer;
+
+  span {
+    width: 24px;
+    height: 3px;
+    background: var(--black);
+    border-radius: 2px;
+  }
+
+  @media ${breakpoints.mobile.large} {
+    display: flex;
+  }
+`;
+
+export const NavSpan = styled.span`
+  cursor: pointer;
+`;
+
+export const ContextMenu = styled.div`
+  display: flex;
+  position: absolute;
+  top: 2em;
+  right: -1.5em;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+  background-color: var(--white);
+  padding: 0.5em 1em;
+  box-shadow: 0 5px 8px -4px var(--light-grey);
+  border-radius: 0 0 5px 5px;
+  z-index: 20;
+`;
+
+export const NavLinkBtn = styled(NavLink)`
+  background-color: var(--accent);
+  color: var(--white);
+  padding: 0.4em 1em;
+  border-radius: 5px;
+  background-image: var(--3d-effect);
+
+  &:hover {
+    filter: brightness(0.9);
+  }
 `;
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,7 +1,16 @@
-import { useContext } from "react";
-import { Link } from "react-router";
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import { HeaderOne } from "@globalStyles";
-import { NavHeader } from "./Nav.styled";
+import {
+  Header,
+  NavBar,
+  NavLink,
+  Burger,
+  NavSection,
+  NavSpan,
+  ContextMenu,
+  NavLinkBtn,
+} from "./Nav.styled";
 import AuthContext from "@contexts/AuthContext";
 
 const Nav = () => {
@@ -12,26 +21,79 @@ const Nav = () => {
   }
 
   const { handleSignOut, user } = auth;
+  const { pathname } = useLocation();
+
+  const [showMenu, setShowMenu] = useState(false);
+  const [burgerOpen, setBurgerOpen] = useState(false);
+
+  useEffect(() => {
+    setShowMenu(false);
+    setBurgerOpen(false);
+  }, [pathname]);
+
+  const handleAvatarClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleBurgerClick = () => {
+    setBurgerOpen(!burgerOpen);
+    setShowMenu(false);
+  };
 
   return (
-    <NavHeader>
-      <nav>
-        <HeaderOne>
-          <a href="/">Palekouran</a>
-        </HeaderOne>
-        <Link to="/">Home</Link>
-        {user?.id ? (
-          <>
-            <Link to="/account">Account</Link>
-            <Link to="/decks">Decks</Link>
-            <span>{user.username}</span>
-            <a onClick={handleSignOut}>Log out</a>
-          </>
-        ) : (
-          <Link to="/login">Log in</Link>
-        )}
-      </nav>
-    </NavHeader>
+    <Header>
+      <NavBar>
+        <NavLink to="/">
+          <HeaderOne>
+            <i className="fa-solid fa-comment-dots"></i>Palekouran
+          </HeaderOne>
+        </NavLink>
+
+        {/* mobile */}
+        <Burger onClick={handleBurgerClick}>
+          <span />
+          <span />
+          <span />
+        </Burger>
+
+        <NavSection $open={burgerOpen}>
+          <NavLink to="/">Home</NavLink>
+          {/* <NavLink to="/about">About</NavLink> */}
+
+          {user?.id ? (
+            <>
+              <NavLink to="/decks">My Decks</NavLink>
+
+              {/* desktop avatar */}
+              {!burgerOpen && (
+                <NavSpan onClick={handleAvatarClick}>
+                  <i className="fa-solid fa-user"></i>
+                  {user.username}
+                </NavSpan>
+              )}
+
+              {/* mobile expanded menu */}
+              {burgerOpen && (
+                <>
+                  <NavLink to="/account">Account</NavLink>
+                  <NavSpan onClick={handleSignOut}>Log out</NavSpan>
+                </>
+              )}
+
+              {/* desktop account dropdown */}
+              {showMenu && !burgerOpen && (
+                <ContextMenu>
+                  <NavLink to="/account">Account</NavLink>
+                  <NavSpan onClick={handleSignOut}>Log out</NavSpan>
+                </ContextMenu>
+              )}
+            </>
+          ) : (
+            <NavLinkBtn to="/login">Log in</NavLinkBtn>
+          )}
+        </NavSection>
+      </NavBar>
+    </Header>
   );
 };
 
